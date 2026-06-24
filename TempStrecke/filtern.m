72c58789ecbb34dadd810750bbc654b3ce19bec3
Frederik <frederik.vonbaeyer@scuderia-mensa.de>
2026-06-24 23:56:57 +0200

    praktikum 2.1 wende tanga

---
+++ TempStrecke/filtern.m
@@ -0,0 +1,270 @@
+
+clear
+clc
+clf
+ 
+MessungenZusammenBauen
+ 
+ 
+%% filtern
+%Messung1
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung1Struct.Afilt = filtfilt(a, b ,Messung1Struct.A);
+Messung1Struct.Bfilt = filtfilt(a, b ,Messung1Struct.B);
+ 
+%Messung1
+for i = 2:Messung2Struct.length
+    if abs(Messung2Struct.A(i,1)-Messung2Struct.A(i-1,1))>=0.03 
+        Messung2Struct.A(i,1) = Messung2Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung2Struct.Afilt = filtfilt(a, b ,Messung2Struct.A);
+Messung2Struct.Bfilt = filtfilt(a, b ,Messung2Struct.B);
+ 
+%Messung3
+for i = 2:Messung3Struct.length
+    if abs(Messung3Struct.A(i,1)-Messung3Struct.A(i-1,1))>=0.03 
+        Messung3Struct.A(i,1) = Messung3Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung3Struct.Afilt = filtfilt(a, b ,Messung3Struct.A);
+Messung3Struct.Bfilt = filtfilt(a, b ,Messung3Struct.B);
+ 
+%Messung4
+for i = 2:Messung4Struct.length
+    if abs(Messung4Struct.A(i,1)-Messung4Struct.A(i-1,1))>=0.03 
+        Messung4Struct.A(i,1) = Messung4Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung4Struct.Afilt = filtfilt(a, b ,Messung4Struct.A);
+Messung4Struct.Bfilt = filtfilt(a, b ,Messung4Struct.B);
+ 
+%Messung6
+for i = 2:Messung6Struct.length
+    if abs(Messung6Struct.A(i,1)-Messung6Struct.A(i-1,1))>=0.03 
+        Messung6Struct.A(i,1) = Messung6Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung6Struct.Afilt = filtfilt(a, b ,Messung6Struct.A);
+Messung6Struct.Bfilt = filtfilt(a, b ,Messung6Struct.B);
+ 
+%Messung7
+for i = 2:Messung7Struct.length
+    if abs(Messung7Struct.A(i,1)-Messung7Struct.A(i-1,1))>=0.03 
+        Messung7Struct.A(i,1) = Messung7Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung7Struct.Afilt = filtfilt(a, b ,Messung7Struct.A);
+Messung7Struct.Bfilt = filtfilt(a, b ,Messung7Struct.B);
+ 
+%Messung8
+for i = 2:Messung8Struct.length
+    if abs(Messung8Struct.A(i,1)-Messung8Struct.A(i-1,1))>=0.05 
+        Messung8Struct.A(i,1) = Messung8Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung8Struct.Afilt = filtfilt(a, b ,Messung8Struct.A);
+Messung8Struct.Bfilt = filtfilt(a, b ,Messung8Struct.B);
+ 
+%Messung9
+for i = 2:Messung9Struct.length
+    if abs(Messung9Struct.A(i,1)-Messung9Struct.A(i-1,1))>=0.09 
+        Messung9Struct.A(i,1) = Messung9Struct.A(i-1,1);
+    end
+end
+[a, b] = butter(2, 8.0000e-04, 'low');
+Messung9Struct.Afilt = filtfilt(a, b ,Messung9Struct.A);
+Messung9Struct.Bfilt = filtfilt(a, b ,Messung9Struct.B);
+ 
+ 
+%% Plotten
+ 
+%Messung1
+figure(1)
+clf
+subplot(3,1,1)
+plot(Messung1Struct.t,Messung1Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung1Struct.t,Messung1Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung1Struct.t,Messung1Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung1Struct.t,Messung1Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung1Struct.t,Messung1Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+ 
+% Messung2
+figure(2)
+clf
+subplot(3,1,1)
+plot(Messung2Struct.t,Messung2Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung2Struct.t,Messung2Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung2Struct.t,Messung2Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung2Struct.t,Messung2Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung2Struct.t,Messung2Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+ 
+% Messung3
+figure(3)
+clf
+subplot(3,1,1)
+plot(Messung3Struct.t,Messung3Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung3Struct.t,Messung3Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung3Struct.t,Messung3Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung3Struct.t,Messung3Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung3Struct.t,Messung3Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+ 
+% Messung4
+figure(4)
+clf
+subplot(3,1,1)
+plot(Messung4Struct.t,Messung4Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung4Struct.t,Messung4Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung4Struct.t,Messung4Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung4Struct.t,Messung4Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung4Struct.t,Messung4Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+ 
+% Messung6
+figure(6)
+clf
+subplot(3,1,1)
+plot(Messung6Struct.t,Messung6Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung6Struct.t,Messung6Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung6Struct.t,Messung6Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung6Struct.t,Messung6Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung6Struct.t,Messung6Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+% Messung3
+figure(7)
+clf
+subplot(3,1,1)
+plot(Messung7Struct.t,Messung7Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung7Struct.t,Messung7Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung7Struct.t,Messung7Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung7Struct.t,Messung7Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung7Struct.t,Messung7Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+ 
+% Messung3
+figure(8)
+clf
+subplot(3,1,1)
+plot(Messung8Struct.t,Messung8Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung8Struct.t,Messung8Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung8Struct.t,Messung8Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung8Struct.t,Messung8Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung8Struct.t,Messung8Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+ 
+% Messung3
+figure(9)
+clf
+subplot(3,1,1)
+plot(Messung9Struct.t,Messung9Struct.A)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung9Struct.t,Messung9Struct.Afilt)
+ 
+subplot(3,1,2)
+plot(Messung9Struct.t,Messung9Struct.B)
+ylabel("voltage")
+xlabel("time [s]")
+hold on 
+plot(Messung9Struct.t,Messung9Struct.Bfilt)
+ 
+subplot(3,1,3)
+plot(Messung9Struct.t,Messung9Struct.C)
+ylabel("voltage")
+xlabel("time [s]")
+ 
+clearvars b a i
---
+++ TempStrecke/versuch1.m
@@ -0,0 +1,253 @@
+clear
+clc
+close all
+
+addpath data\
+
+%% Messungen zusammen bauen
+
+% Stellsprungsmessung zusammen bauen:
+Messung1Struct = struct("A",[],"B",[],"C",[],"dt",0.002,"length",[],"t",[]);
+for i=1:2
+    filename = sprintf("stellsprungversuch_%1d.mat",i);
+    load(filename)
+    Messung1Struct.A = [Messung1Struct.A;A];
+    Messung1Struct.B = [Messung1Struct.B;B];
+    Messung1Struct.C = [Messung1Struct.C;C];
+    [Messung1Struct.length,~] = size(Messung1Struct.A);
+    Messung1Struct.t = [1:Messung1Struct.length]'.*0.002;
+end
+
+stellsprungStruct = Messung1Struct; 
+
+
+% Stoersprungsmessung zusammen bauen:
+Messung2Struct = struct("A",[],"B",[],"C",[],"dt",0.002,"length",[]);
+
+filename = sprintf("störsprungversuch");
+load(filename)
+Messung2Struct.A = [Messung2Struct.A;A];
+Messung2Struct.B = [Messung2Struct.B;B];
+Messung2Struct.C = [Messung2Struct.C;C];
+[Messung2Struct.length,~] = size(Messung2Struct.A);
+Messung2Struct.t = [1:Messung2Struct.length]'.*0.002;
+
+stoersprungStruct = Messung2Struct;
+
+
+%% wichtige Parameter finden:
+[~, stellsprungStruct.sprungIndex] = max(gradient(stellsprungStruct.A));
+[~, stoersprungStruct.sprungIndex] = max(gradient(stoersprungStruct.C));
+clearvars -except stellsprungStruct stoersprungStruct
+
+
+
+%% Plot the filtered signals on the current plot:
+
+[a, b] = butter(2, 8.0000e-04, 'low');
+stellsprungStruct.Afilt = filtfilt(a, b ,stellsprungStruct.A);
+stellsprungStruct.Bfilt = filtfilt(a, b ,stellsprungStruct.B);
+stellsprungStruct.Cfilt = filtfilt(a, b ,stellsprungStruct.C);
+
+[a, b] = butter(2, 8.0000e-04, 'low');
+stoersprungStruct.Afilt = filtfilt(a, b ,stoersprungStruct.A);
+stoersprungStruct.Bfilt = filtfilt(a, b ,stoersprungStruct.B);
+stoersprungStruct.Cfilt = filtfilt(a, b ,stoersprungStruct.C);
+
+
+%% Plot them baddies raw (and zero order filtered)
+
+figure(10)
+hold on
+grid on
+
+p11 = subplot(3,1,1);
+hold on
+plot(p11, stellsprungStruct.A)
+title Stellgroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+
+p12 = subplot(3,1,2);
+hold on
+plot(p12, stellsprungStruct.B)
+plot(p12, stellsprungStruct.Bfilt )
+title Regelgroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+legend show
+
+p13 = subplot(3,1,3);
+hold on
+plot(p13, stellsprungStruct.C)
+title stoergroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+
+linkaxes([p11 p12 p13], 'xy');
+
+
+figure(20)
+hold on
+grid on
+
+p21 = subplot(3,1,1);
+plot(p21, stoersprungStruct.A)
+title Stellgroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+
+p22 = subplot(3,1,2);
+hold on
+plot(p22, stoersprungStruct.B)
+plot(p22, stoersprungStruct.Bfilt)
+title Regelgroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+legend show
+
+p23 = subplot(3,1,3);
+plot(p23, stoersprungStruct.C)
+title Stoergroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+
+linkaxes([p21 p22 p23], 'xy');
+
+
+%% Wendetanga verfahren
+% Wendlerpunkt und geradengleichung der Tanga:
+stellsprungStruct.BfiltD = gradient(stellsprungStruct.Bfilt);
+[M, I] = max(stellsprungStruct.BfiltD);
+B = stellsprungStruct.Bfilt(I) - M * I;
+stellsprungStruct.wendeTanga = M * (1:1:length(stellsprungStruct.A)) + B;
+
+
+figure(30)
+
+p31 = subplot(3,1,1);
+hold on
+plot(p31, stellsprungStruct.Bfilt)
+title Regelgroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+
+p32 = subplot(3,1,2);
+hold on
+plot(p32, stellsprungStruct.BfiltD)
+plot(I,M,'x')
+title("Gradient der Regelgroesse")
+xlabel("time [s]")
+legend show
+
+p33 = subplot(3,1,3);
+hold on
+plot(p33, stellsprungStruct.Bfilt)
+plot(p33, I, stellsprungStruct.Bfilt(I), 'x')
+plot(p33, stellsprungStruct.wendeTanga)
+title stoergroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+ylim([4.5 6])
+
+linkaxes([p11 p12 p13], 'x');
+
+
+
+% wendetanga werte ablesen:
+figure(32)
+clf
+hold on
+plot(stellsprungStruct.Bfilt)
+plot(I, stellsprungStruct.Bfilt(I), 'x')
+plot(stellsprungStruct.wendeTanga)
+title("Stellgroessenwendetanga")
+xlabel("samples ")
+ylabel("voltage [V]")
+ylim([4.5 6.2])
+xlim([0 30000])
+yline(max(stellsprungStruct.Bfilt))
+yline(stellsprungStruct.Bfilt(stellsprungStruct.sprungIndex))
+xline(stellsprungStruct.sprungIndex)
+
+% schnittpunkte:
+% naehster index der Tanga an den schnittpunkten:
+[~, indexUpper] = min(abs(stellsprungStruct.wendeTanga - max(stellsprungStruct.Bfilt)));
+[~, indexLower] = min(abs(stellsprungStruct.wendeTanga - stellsprungStruct.Bfilt(stellsprungStruct.sprungIndex)));
+
+plot(indexUpper, stellsprungStruct.wendeTanga(indexUpper),'o',LineWidth=5)
+plot(indexLower, stellsprungStruct.wendeTanga(indexLower),'o',LineWidth=5)
+
+% werte Ausgeben:
+stellsprungStruct.T1 = (indexLower - stellsprungStruct.sprungIndex) * stellsprungStruct.dt;
+stellsprungStruct.T2 = (indexUpper - indexLower) * stellsprungStruct.dt
+
+
+
+
+
+
+% Wendlerpunkt und geradengleichung der Tanga:
+stoersprungStruct.BfiltD = gradient(stoersprungStruct.Bfilt);
+[M, I] = min(stoersprungStruct.BfiltD);
+B = stoersprungStruct.Bfilt(I) - M * I;
+stoersprungStruct.wendeTanga = M * (1:1:length(stoersprungStruct.A)) + B;
+
+
+figure(40)
+
+p31 = subplot(3,1,1);
+hold on
+plot(p31, stoersprungStruct.Bfilt)
+title Regelgroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+
+p32 = subplot(3,1,2);
+hold on
+plot(p32, stoersprungStruct.BfiltD)
+plot(I, M,'x')
+title("Gradient der Regelgroesse")
+xlabel("time [s]")
+legend show
+
+p33 = subplot(3,1,3);
+hold on
+plot(p33, stoersprungStruct.Bfilt)
+plot(p33, I, stoersprungStruct.Bfilt(I), 'x')
+plot(p33, stoersprungStruct.wendeTanga)
+title stoergroesse
+xlabel("time [s]")
+ylabel("voltage [V]")
+ylim([4.5 6])
+
+linkaxes([p31 p32 p33], 'x');
+
+
+% wendetanga werte ablesen:
+figure(41)
+clf
+hold on
+plot(stoersprungStruct.Bfilt)
+plot(I, stoersprungStruct.Bfilt(I), 'x')
+plot(stoersprungStruct.wendeTanga)
+title("stoergroessenwendetanga")
+xlabel("samples ")
+ylabel("voltage [V]")
+ylim([4.8 6])
+xlim([0 30000])
+yline(min(stoersprungStruct.Bfilt))
+yline(stoersprungStruct.Bfilt(stoersprungStruct.sprungIndex))
+xline(stoersprungStruct.sprungIndex)
+
+% schnittpunkte:
+% naehster index der Tanga an den schnittpunkten:
+[~, indexUpper] = min(abs(stoersprungStruct.wendeTanga - min(stoersprungStruct.Bfilt)));
+[~, indexLower] = min(abs(stoersprungStruct.wendeTanga - stoersprungStruct.Bfilt(stoersprungStruct.sprungIndex)));
+
+plot(indexUpper, stoersprungStruct.wendeTanga(indexUpper),'o',LineWidth=5)
+plot(indexLower, stoersprungStruct.wendeTanga(indexLower),'o',LineWidth=5)
+
+% werte Ausgeben:
+stoersprungStruct.T1 = (indexLower - stoersprungStruct.sprungIndex) * stoersprungStruct.dt;
+stoersprungStruct.T2 = (indexUpper - indexLower) * stoersprungStruct.dt
